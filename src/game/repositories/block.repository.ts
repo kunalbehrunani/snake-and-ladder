f536c40b7150f5e1a15a7775967ab688feb22278
kunalbehrunani <behrunanik@gmail.com>
2024-07-27 02:43:26 +0530

    fix: removed obsolete file - block repository

--- src/game/repositories/block.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Block } from '../schemas/block.schema';
-
-@Injectable()
-export class BlockRepository {
-  createNewBlock(param: { position: number }): Block {
-    return new Block(param.position);
-  }
-}
